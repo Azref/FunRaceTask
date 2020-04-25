23ea6b66988cb5d5f50c303ada21da928a2c9e33
Azref <cuneyt.taskent@gmail.com>
2020-04-25 13:03:42 +0300

    Ragdoll is added.

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public Transform Target;
+
+    [Range(0.1f, 1f)]
+    public float SmoothFactor = .5f;
+
+    public float LookFrontDistance = 2f;
+
+    private Vector3 _followDistance;
+
+    private Vector3 _velocity;
+
+    void Start()
+    {
+        _followDistance = transform.position - Target.position;
+    }
+
+    void Update()
+    {
+        Move();
+
+        Look();
+    }
+
+    private void Look()
+    {
+        transform.LookAt(Target.position + Target.forward * LookFrontDistance);
+    }
+
+    public void Move(bool soft = true)
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, Target.position + _followDistance, ref _velocity, SmoothFactor);
+    }
+}
---
+++ Assets/Scripts/RagdollActivater.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagdollActivater : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Obstacle"))
+        {
+        Debug.Log("++ OnTriggerEnter");
+
+        GetComponent<CapsuleCollider>().enabled = false;
+        GetComponent<Animator>().enabled = false;
+        }
+    }
+
+    private void Reset()
+    {
+        GetComponent<CapsuleCollider>().enabled = true;
+        GetComponent<Animator>().enabled = true;
+    }
+}
